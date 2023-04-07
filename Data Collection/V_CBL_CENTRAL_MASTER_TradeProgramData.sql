039ddac5f9305fdbe9f2e132d00bae65325bc6d9
votana srey <votana.srey@heineken.com>
2023-04-07 20:38:56 +0700

    adding the folders and file v1

---
+++ Data Collection/V_CBL_CENTRAL_MASTER_TradeProgramData.sql
@@ -0,0 +1,3 @@
+--contract by outlet
+SELECT * FROM 
+[192.168.22.1\CBLSQL].[CBL_CENTRAL_2015].[dbo].[CBL CENTRAL MASTER$Contract Header]
---
+++ Data Collection/V_TrackingSalesVolumeTargetOutletSponsorship.sql
@@ -0,0 +1,2 @@
+SELECT *
+FROM [192.168.22.1\CBLSQL].[DIS_Reports].[dbo].[Tracking Sales Volume Target outlet sponsorship]
